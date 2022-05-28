be0b7f369ad2af6a88cffe2e95714c6b810667f3
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-05-28 12:57:10 +0200

    udpate

--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -2,10 +2,11 @@ import React, { useCallback, useEffect, useState } from "react";
 import useFetch from "../../Hooks/useFetch";
 import Genres from "../Genres/Genres";
 import MovieItem from "./MovieItem/MovieItem";
-const DefualtValueGenres = JSON.parse(localStorage.getItem("genres"))
-	? JSON.parse(localStorage.getItem("genres")).id
-	: "";
+
 function MoviesList() {
+	const DefualtValueGenres = JSON.parse(localStorage.getItem("genres"))
+		? JSON.parse(localStorage.getItem("genres")).id
+		: "";
 	const [genres, setGenres] = useState(DefualtValueGenres);
 	const [sortingBy, setSortingBy] = useState("popularity.desc");
 	const [url, setUrl] = useState(
@@ -20,6 +21,7 @@ function MoviesList() {
 		setUrl(
 			`https://api.themoviedb.org/3/discover/movie?api_key=d948c5c0ea05d8b074392d5c6641f56c&language=en-US&region=us&sort_by=${sortingBy}&include_adult=false&include_video=true&page=1&with_genres=${genres}&with_watch_monetization_types=flatrate`,
 		);
+		console.log();
 	}, [url, genres, sortingBy, isLoading, error]);
 	let movies = responseData.results;
 
