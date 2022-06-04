1653c1cb2fdd9a470363bd06f75f449777347af0
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-06-04 14:56:30 +0200

    add loading LoadingSpinner for home page

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -7,37 +7,44 @@ import Carousel from "../UI/Carousel/Carousel";
 import Container from "../UI/Container/Container";
 import "./Herosection.css";
 import Banner from "./Banner/Banner";
+import LoadingSpinner from "../UI/LoadingSpinner/LoadingSpinner";
 function HeroSection({ openModalHandler }) {
-	const response =
+	const [responseData, isLoading, error] =
 		useFetch(`https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.REACT_APP_TMDB_API_KEY}&&page=1
 	`);
-	const trending = response[0].results;
+	// const trending = response[0].results;
+	let trending = responseData.results;
 	const [background, setBackground] = useState("");
 	const setBackgroundHandler = (url) => {
 		setBackground(url);
 	};
 
 	return (
-		<div
-			className="HeroSection"
-			style={{
-				backgroundImage: `url(${background})`,
-			}}>
-			{trending && (
-				<Container>
-					<Carousel
-						data={trending}
-						slidesCount={1}
-						spaceBetween={0}
-						centeredSlides={true}
-						fn={setBackgroundHandler}
-						modules={[Autoplay, Navigation]}
-						delay={7000}>
-						<Banner />
-					</Carousel>
-				</Container>
+		<>
+			{isLoading && <LoadingSpinner />}
+			{trending && !isLoading && (
+				<div
+					className="HeroSection"
+					style={{
+						backgroundImage: `url(${background})`,
+					}}>
+					{trending && (
+						<Container>
+							<Carousel
+								data={trending}
+								slidesCount={1}
+								spaceBetween={0}
+								centeredSlides={true}
+								fn={setBackgroundHandler}
+								modules={[Autoplay, Navigation]}
+								delay={7000}>
+								<Banner />
+							</Carousel>
+						</Container>
+					)}
+				</div>
 			)}
-		</div>
+		</>
 	);
 }
 
