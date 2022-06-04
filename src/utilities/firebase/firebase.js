7333d735991171b52a4d2dd97aaa5897017c9a0d
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-06-04 13:54:19 +0200

    update

--- src/utilities/firebase/firebase.js
+++ src/utilities/firebase/firebase.js
@@ -13,13 +13,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: "AIzaSyBr2_2Q77XsByGHoU__4v4kQYcDeysB8Jw",
-	authDomain: "movie-app-99180.firebaseapp.com",
-	databaseURL: "https://movie-app-99180-default-rtdb.firebaseio.com",
-	projectId: "movie-app-99180",
-	storageBucket: "movie-app-99180.appspot.com",
-	messagingSenderId: "99775019986",
-	appId: "1:99775019986:web:3718ae83a08a1000dae60b",
+	apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+	authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+	databaseURL: `${process.env.REACT_APP_DATABASE_URL}`,
+	projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+	storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+	messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+	appId: `${process.env.REACT_APP_APP_ID}`,
 };
 
 // Initialize Firebase
