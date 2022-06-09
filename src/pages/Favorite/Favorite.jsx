fcd27cfda0bcf869b7f2ff0df1050108ac4a56ae
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-06-09 16:45:07 +0200

    update

--- src/pages/Favorite/Favorite.jsx
+++ src/pages/Favorite/Favorite.jsx
@@ -17,7 +17,7 @@ function Favorite() {
 		<section className="Favourite">
 			<Container>
 				<div className="row">
-					{loading !== false && <LoadingSpinner></LoadingSpinner>}
+					{loading && <LoadingSpinner></LoadingSpinner>}
 					{FavoriteItems.length === 0 && !loading && (
 						<div className="col notDataFound">Favorite Items is empty</div>
 					)}
