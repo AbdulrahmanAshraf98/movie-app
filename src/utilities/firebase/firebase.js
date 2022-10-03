07a1a9e30fb6cadea9abd2bad5959207f00681b9
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-10-03 22:57:01 +0200

    Remove ConsoleLog

--- src/utilities/firebase/firebase.js
+++ src/utilities/firebase/firebase.js
@@ -22,7 +22,6 @@ import {
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
