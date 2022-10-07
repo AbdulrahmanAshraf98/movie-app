c55b4b431b9a0d6135a4fd776442abe12df939d7
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-10-08 00:10:33 +0200

    Update style Search Feed Component

--- src/components/SearchResultsListPreview/SearchResultListPreview.jsx
+++ src/components/SearchResultsListPreview/SearchResultListPreview.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import SearchResultItem from "./SearchResultsCard/SearchResultItem";
-import classes from "./SearchResultListPreview.module.css";
 const SearchResultListPreview = ({ data }) => {
 	return data.map((item) => (
-		<div className={`${classes.col}`} key={item.id}>
+		<div className="col" key={item.id}>
 			<SearchResultItem item={item} />
 		</div>
 	));
