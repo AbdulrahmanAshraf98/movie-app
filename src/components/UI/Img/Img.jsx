6128d25347aae6d91c305a6a59bae065c77d763e
AbdulrahmanAshraf98 <abdulrahmanashraf9898@gmail.com>
2022-10-07 23:33:57 +0200

    remove scroll tracker

--- src/components/UI/Img/Img.jsx
+++ src/components/UI/Img/Img.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import {
 	LazyLoadImage,
-	trackWindowScroll,
 } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
-function Img({ className, src, alt, children, scrollPosition }) {
+function Img({ className, src, alt, children, }) {
 	return (
 		<div className={className}>
 			<LazyLoadImage
@@ -14,11 +13,11 @@ function Img({ className, src, alt, children, scrollPosition }) {
 				src={src}
 				wrapperClassName={className}
 				threshold={200}
-				scrollPosition={scrollPosition}
+				
 			/>
 			{children}
 		</div>
 	);
 }
 
-export default trackWindowScroll(Img);
+export default Img;
